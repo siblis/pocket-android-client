8db9a97f74f36b116e444e3f52ea429fa809a03c
Wintermute46 <r.tzymbalyuk@yandex.ru>
2018-08-24 01:29:38 +0300

    Добавлен код Александра Коровникова

--- app/src/main/java/com/gb/pocketmessenger/SettingsActivity.java
+++ app/src/main/java/com/gb/pocketmessenger/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.gb.pocketmessenger;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
     }
 }
